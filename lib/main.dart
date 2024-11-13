91bc5d2a4ba87756d046ce29371466f57f181ece
satrujit11 <beherasatrujit12@gmail.com>
2024-11-13 16:16:34 +0530

    fiix: permission

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Bluetooth Printer Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -32,46 +32,54 @@ class MyHome extends StatefulWidget {
 
 class _MyHomeState extends State<MyHome> {
   bool isPermissionGranted = false;
-
   List<BluetoothInfo> devices = [];
-  
   BluetoothInfo? connectedDevice;
+  String? connectingToDevice;
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(
-      (_) => _checkPermissions(),
-    );
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkPermissions());
   }
 
   Future<void> _checkPermissions() async {
-    bool bluetoothGranted = await Permission.bluetooth.isGranted;
-    bool nearbyDevicesGranted = await Permission.nearbyWifiDevices.isGranted;
+    // Request all required Bluetooth permissions
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.bluetooth,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan,
+      Permission.nearbyWifiDevices,
+    ].request();
 
-    if (!bluetoothGranted /*|| !bluetoothConnectGranted || !bluetoothScanGranted */ ||
-        !nearbyDevicesGranted) {
-      await [
-        Permission.bluetooth,
-        Permission.nearbyWifiDevices,
-      ].request();
-    }
+    // Check if all necessary permissions are granted
+    bool allGranted = statuses.values.every((status) => status.isGranted);
 
-    // Update permission state
+    // Update state with permission result
     setState(() {
-      isPermissionGranted =
-          bluetoothGranted && /* bluetoothConnectGranted && bluetoothScanGranted && */
-              nearbyDevicesGranted;
+      isPermissionGranted = allGranted;
     });
 
-    // Check if Bluetooth is enabled
-    final bool isBluetoothEnabled =
-        await PrintBluetoothThermal.bluetoothEnabled;
-    debugPrint("Bluetooth Enabled: $isBluetoothEnabled");
+    if (isPermissionGranted) {
+      // Check if Bluetooth is enabled
+      final bool isBluetoothEnabled =
+          await PrintBluetoothThermal.bluetoothEnabled;
+      debugPrint("Bluetooth Enabled: $isBluetoothEnabled");
+
+      if (!isBluetoothEnabled) {
+        debugPrint("Bluetooth is disabled. Please enable it.");
+      }
+    } else {
+      debugPrint("Required permissions are not granted.");
+    }
   }
 
   Future<void> pairedBluetooths() async {
     debugPrint("Getting paired Bluetooth devices");
     final result = await PrintBluetoothThermal.pairedBluetooths;
+    setState(() {
+      devices = result;
+    });
+
     if (result.isEmpty) {
       debugPrint("No Bluetooth devices found");
     } else {
@@ -79,30 +87,65 @@ class _MyHomeState extends State<MyHome> {
         debugPrint("Device Name: ${info.name}");
       }
     }
-    setState(() {
-      devices = result;
-    });
   }
 
   Future<bool> connect(BluetoothInfo device) async {
-    bool result =
-        await PrintBluetoothThermal.connect(macPrinterAddress: device.macAdress);
+    setState(() {
+      connectingToDevice = device.macAdress;
+    });
+    bool result = await PrintBluetoothThermal.connect(
+        macPrinterAddress: device.macAdress);
     if (result) {
       setState(() {
         connectedDevice = device;
       });
     }
+    setState(() {
+      connectingToDevice = null;
+    });
     return result;
+  }
+
+  Future<void> printTest() async {
+    if (connectedDevice == null) {
+      debugPrint("No device connected");
+      return;
+    }
+    // Perform printing logic here
+    debugPrint("Printing test page...");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(isPermissionGranted
-            ? 'Permissions granted'
-            : 'Permissions not granted'),
-      ),
+          title: Text(isPermissionGranted
+              ? 'Permissions Granted'
+              : 'Permissions Not Granted'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.print),
+              onPressed: isPermissionGranted && connectedDevice != null
+                  ? () async {
+                      await printTest();
+                    }
+                  : null,
+            ),
+            IconButton(
+              icon: const Icon(Icons.bluetooth),
+              onPressed: isPermissionGranted
+                  ? () async {
+                      await pairedBluetooths();
+                    }
+                  : null,
+            ),
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () async {
+                await _checkPermissions();
+              },
+            ),
+          ]),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -110,7 +153,9 @@ class _MyHomeState extends State<MyHome> {
           children: <Widget>[
             ...devices.map(
               (device) => ListTile(
-                trailing: connectedDevice?.macAdress == device.macAdress ? const Icon(Icons.check_circle, color: Colors.green): null,
+                trailing: connectingToDevice == device.macAdress ? const CircularProgressIndicator() : connectedDevice?.macAdress == device.macAdress
+                    ? const Icon(Icons.check_circle, color: Colors.green)
+                    : null,
                 title: Text(device.name),
                 subtitle: Text(device.macAdress),
                 onTap: () async {
@@ -127,19 +172,18 @@ class _MyHomeState extends State<MyHome> {
                       ? () async {
                           await pairedBluetooths();
                         }
-                      : null, // Disable button if permissions are not granted
+                      : null,
                   child: const Text('Get Paired Devices'),
                 ),
-                if (connectedDevice != null) ...[
-                OutlinedButton(
-                  onPressed: isPermissionGranted
-                      ? () async {
-                          await printTest();
-                        }
-                      : null, // Disable button if permissions are not granted
-                  child: const Text('Print'),
-                ),
-                ],
+                if (connectedDevice != null)
+                  OutlinedButton(
+                    onPressed: isPermissionGranted
+                        ? () async {
+                            await printTest();
+                          }
+                        : null,
+                    child: const Text('Print'),
+                  ),
               ],
             ),
           ],
@@ -149,86 +193,93 @@ class _MyHomeState extends State<MyHome> {
   }
 }
 
-
 Future<void> printTest() async {
-    bool conecctionStatus = await PrintBluetoothThermal.connectionStatus;
-    if (conecctionStatus) {
-      List<int> ticket = await testTicket();
-      final result = await PrintBluetoothThermal.writeBytes(ticket);
-      print("print result: $result");
-    } else {
-      //no connected
-    }
+  bool conecctionStatus = await PrintBluetoothThermal.connectionStatus;
+  if (conecctionStatus) {
+    List<int> ticket = await testTicket();
+    final result = await PrintBluetoothThermal.writeBytes(ticket);
+    print("print result: $result");
+  } else {
+    //no connected
+  }
 }
 
 Future<List<int>> testTicket() async {
-    List<int> bytes = [];
-    // Using default profile
-    final profile = await CapabilityProfile.load();
-    final generator = Generator(PaperSize.mm58, profile);
-    //bytes += generator.setGlobalFont(PosFontType.fontA);
-    bytes += generator.reset();
+  List<int> bytes = [];
+  // Using default profile
+  final profile = await CapabilityProfile.load();
+  final generator = Generator(PaperSize.mm58, profile);
+  //bytes += generator.setGlobalFont(PosFontType.fontA);
+  bytes += generator.reset();
 
-    bytes += generator.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ', styles: const PosStyles());
-    bytes += generator.text('Special 1: ñÑ àÀ èÈ éÉ üÜ çÇ ôÔ', styles: const PosStyles(codeTable: 'CP1252'));
-    bytes += generator.text(
-      'Special 2: blåbærgrød',
-      styles: const PosStyles(codeTable: 'CP1252'),
-    );
+  bytes += generator.text(
+      'Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ',
+      styles: const PosStyles());
+  bytes += generator.text('Special 1: ñÑ àÀ èÈ éÉ üÜ çÇ ôÔ',
+      styles: const PosStyles(codeTable: 'CP1252'));
+  bytes += generator.text(
+    'Special 2: blåbærgrød',
+    styles: const PosStyles(codeTable: 'CP1252'),
+  );
 
-    bytes += generator.text('Bold text', styles: const PosStyles(bold: true));
-    bytes += generator.text('Reverse text', styles: const PosStyles(reverse: true));
-    bytes += generator.text('Underlined text', styles: const PosStyles(underline: true), linesAfter: 1);
-    bytes += generator.text('Align left', styles: const PosStyles(align: PosAlign.left));
-    bytes += generator.text('Align center', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text('Align right', styles: const PosStyles(align: PosAlign.right), linesAfter: 1);
+  bytes += generator.text('Bold text', styles: const PosStyles(bold: true));
+  bytes +=
+      generator.text('Reverse text', styles: const PosStyles(reverse: true));
+  bytes += generator.text('Underlined text',
+      styles: const PosStyles(underline: true), linesAfter: 1);
+  bytes += generator.text('Align left',
+      styles: const PosStyles(align: PosAlign.left));
+  bytes += generator.text('Align center',
+      styles: const PosStyles(align: PosAlign.center));
+  bytes += generator.text('Align right',
+      styles: const PosStyles(align: PosAlign.right), linesAfter: 1);
 
-    bytes += generator.row([
-      PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-      PosColumn(
-        text: 'col6',
-        width: 6,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-      PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-    ]);
+  bytes += generator.row([
+    PosColumn(
+      text: 'col3',
+      width: 3,
+      styles: const PosStyles(align: PosAlign.center, underline: true),
+    ),
+    PosColumn(
+      text: 'col6',
+      width: 6,
+      styles: const PosStyles(align: PosAlign.center, underline: true),
+    ),
+    PosColumn(
+      text: 'col3',
+      width: 3,
+      styles: const PosStyles(align: PosAlign.center, underline: true),
+    ),
+  ]);
 
-    //barcode
-    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
-    bytes += generator.barcode(Barcode.upcA(barData));
+  //barcode
+  final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
+  bytes += generator.barcode(Barcode.upcA(barData));
 
-    //QR code
-    bytes += generator.qrcode('example.com');
+  //QR code
+  bytes += generator.qrcode('example.com');
 
-    bytes += generator.text(
-      'Text size 50%',
-      styles: const PosStyles(
-        fontType: PosFontType.fontB,
-      ),
-    );
-    bytes += generator.text(
-      'Text size 100%',
-      styles: const PosStyles(
-        fontType: PosFontType.fontA,
-      ),
-    );
-    bytes += generator.text(
-      'Text size 200%',
-      styles: const PosStyles(
-        height: PosTextSize.size2,
-        width: PosTextSize.size2,
-      ),
-    );
+  bytes += generator.text(
+    'Text size 50%',
+    styles: const PosStyles(
+      fontType: PosFontType.fontB,
+    ),
+  );
+  bytes += generator.text(
+    'Text size 100%',
+    styles: const PosStyles(
+      fontType: PosFontType.fontA,
+    ),
+  );
+  bytes += generator.text(
+    'Text size 200%',
+    styles: const PosStyles(
+      height: PosTextSize.size2,
+      width: PosTextSize.size2,
+    ),
+  );
 
-    bytes += generator.feed(2);
-    //bytes += generator.cut();
-    return bytes;
+  bytes += generator.feed(2);
+  //bytes += generator.cut();
+  return bytes;
 }
